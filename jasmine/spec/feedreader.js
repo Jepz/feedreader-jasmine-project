032c1131ce4e03ae9ebb78bb91c3dee37831df22
jepz <jesper.lindholmer@avaloninnovation.com>
2016-10-18 23:02:43 +0200

    jsbeautifier the JS and added some more

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -8,94 +8,97 @@ $(function() {
         });
 
         /*
-        *Go over each feed and check that URL is not empty
-        */
-         it('has URL', function(){
-           for (var i = 0; i < allFeeds.length; i++) {
-             expect(allFeeds[i].url).not.toBe('');
-           }
-         });
+         *Go over each feed and check that URL is not empty
+         */
+        it('has URL', function() {
+            for (var i = 0; i < allFeeds.length; i++) {
+                expect(allFeeds[i].url).toBeDefined();
+                expect(allFeeds[i].url).not.toBe('');
+            }
+        });
 
-         /*
+        /*
          *Similare as above, but for Name.
          */
 
-         it('has a Name', function(){
-           for (var i = 0; i < allFeeds.length; i++) {
-             expect(allFeeds[i].name).not.toBe('');
-           }
-         });
+        it('has a Name', function() {
+            for (var i = 0; i < allFeeds.length; i++) {
+                expect(allFeeds[i].url).toBeDefined();
+                expect(allFeeds[i].name).not.toBe('');
+            }
+        });
 
     });
 
 
     describe('The Menu', function() {
 
-      /*
-      * Looking at body and checking if it has the class 'menu-hidden'
-      */
-       it('Menu is hidden by default', function() {
-         expect($('body').hasClass('menu-hidden')).toBe(true);
-       });
+        /*
+         * Looking at body and checking if it has the class 'menu-hidden'
+         */
+        it('Menu is hidden by default', function() {
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
 
-       /*
-       * Added an variable to easier access what I call hamburger icon.
-       */
+        /*
+         * Added an variable to easier access what I call hamburger icon.
+         */
         var $hamburgerIcon = $('.menu-icon-link');
 
         /*
-        * When the icon is clicked, with jquery, i excpect the class 'menu-hidden' to be removed.
-        * Second click it's expected to have the class.
-        */
+         * When the icon is clicked, with jquery, i excpect the class 'menu-hidden' to be removed.
+         * Second click it's expected to have the class.
+         */
         it('Menu is toggled by click', function() {
-          $hamburgerIcon.click();
-          expect($('body').hasClass('menu-hidden')).toBe(false);
+            $hamburgerIcon.click();
+            expect($('body').hasClass('menu-hidden')).toBe(false);
 
-          $hamburgerIcon.click();
-          expect($('body').hasClass('menu-hidden')).toBe(true);
+            $hamburgerIcon.click();
+            expect($('body').hasClass('menu-hidden')).toBe(true);
         });
 
     });
 
 
-    describe('Initial Entries', function(){
+    describe('Initial Entries', function() {
 
-      /*
-      * When the load is done, loadFeed is called.
-      */
-       beforeEach(function(done) {
-               loadFeed(0, done);
-       });
+        /*
+         * When the load is done, loadFeed is called.
+         */
+        beforeEach(function(done) {
+            loadFeed(0, done);
+        });
 
-       it('Feed has atleast one entry', function(done) {
-           expect($('.entry').length).toBeGreaterThan(0);
-       });
+        it('Feed has atleast one entry', function(done) {
+            var entry = $('.feed .entry')[0];
+            expect($('entry').length).toBeGreaterThan(0);
+        });
 
-     });
+    });
 
-     describe('New Feed Selection', function(){
+    describe('New Feed Selection', function() {
 
 
-       /*
-       * Initial loaded setup
-       */
+        /*
+         * Initial loaded setup
+         */
         var initFeed;
-       beforeEach(function(done) {
-         loadFeed(0, function(){
+        beforeEach(function(done) {
+            loadFeed(0, function() {
 
-           initFeed = $('.feed').find('.entry-link').attr('href');
-           loadFeed(1, function(){
-             done();
-           });
+                initFeed = $('.feed').find('.entry-link').attr('href');
+                loadFeed(1, function() {
+                    done();
+                });
 
-         });
-       });
+            });
+        });
 
         it('Feed has been changed', function() {
 
-          var newFeed = $('.feed').find('.entry-link').attr('href');
-          expect(newFeed).not.toEqual(initFeed);
+            var newFeed = $('.feed').find('.entry-link').attr('href');
+            expect(newFeed).not.toEqual(initFeed);
 
         });
-     });
+    });
 }());
